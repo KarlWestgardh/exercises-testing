966a60799c04782e365e79f654f879c9c672a0f8
NiklasMoller <niklasxmoller@gmail.com>
2020-02-06 12:23:51 +0100

    Added template for tests

---
+++ CoffeeMaker_JUnit/src/unittests/edu/ncsu/csc326/InventoryTest.java
@@ -0,0 +1,40 @@
+package unittests.edu.ncsu.csc326;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class InventoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addChocolate() {
+    }
+
+    @Test
+    void addCoffee() {
+    }
+
+    @Test
+    void addMilk() {
+    }
+
+    @Test
+    void addSugar() {
+    }
+
+    @Test
+    void enoughIngredients() {
+    }
+
+    @Test
+    void useIngredients() {
+    }
+}
---
+++ CoffeeMaker_JUnit/src/unittests/edu/ncsu/csc326/coffeemaker/unittests/edu/ncsu/csc326/RecipeBookTest.java
@@ -0,0 +1,30 @@
+package unittests.edu.ncsu.csc326;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeBookTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addRecipe() {
+    }
+
+    @Test
+    void deleteRecipe() {
+    }
+
+    @Test
+    void editRecipe() {
+    }
+}
