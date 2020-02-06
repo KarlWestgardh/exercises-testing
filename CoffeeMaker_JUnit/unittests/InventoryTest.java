e582d7604603fee878a7c4d26fe5ca163eb0adec
NiklasMoller <niklasxmoller@gmail.com>
2020-02-06 12:47:52 +0100

    Refactoring of code

--- CoffeeMaker_JUnit/unittests/InventoryTest.java
+++ CoffeeMaker_JUnit/unittests/InventoryTest.java
@@ -1,5 +1,3 @@
-package unittests.edu.ncsu.csc326;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
--- CoffeeMaker_JUnit/unittests/RecipeBookTest.java
+++ CoffeeMaker_JUnit/unittests/RecipeBookTest.java
@@ -1,10 +1,6 @@
-package unittests.edu.ncsu.csc326;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class RecipeBookTest {
 
