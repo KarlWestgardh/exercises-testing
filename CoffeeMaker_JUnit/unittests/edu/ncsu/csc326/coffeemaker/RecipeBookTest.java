1b64f5672142a72b5bd077077f4d0fb9bc9797a6
karlwes <karl.westgardh@gmail.com>
2020-02-06 09:05:37 +0100

    add editrecipetest

--- CoffeeMaker_JUnit/unittests/edu/ncsu/csc326/coffeemaker/RecipeBookTest.java
+++ CoffeeMaker_JUnit/unittests/edu/ncsu/csc326/coffeemaker/RecipeBookTest.java
@@ -1,8 +1,8 @@
 package edu.ncsu.csc326.coffeemaker;
-import junit.framework.TestCase;
-import java.util.Arrays; 
-import java.util.*; 
 
+import junit.framework.TestCase;
+import java.util.Arrays;
+import java.util.*;
 
 /**
  * 
@@ -10,12 +10,13 @@ import java.util.*;
  * @author Niklas Möller
  * @author Karl Westgårdh
  *
- * Unit tests for Main Functionality
+ *         Unit tests for Main Functionality
  */
 public class RecipeBookTest extends TestCase {
 
 	private CoffeeMaker cm;
 	private Recipe r1;
+	private Recipe r2;
 	private Recipe[] recipeBook;
 	HashSet<Recipe> h;
 
@@ -23,7 +24,7 @@ public class RecipeBookTest extends TestCase {
 	protected void setUp() throws Exception {
 		cm = new CoffeeMaker();
 
-		//Set up for r1
+		// Set up for r1
 		r1 = new Recipe();
 		r1.setName("Coffee");
 		r1.setAmtChocolate("0");
@@ -32,62 +33,105 @@ public class RecipeBookTest extends TestCase {
 		r1.setAmtSugar("1");
 		r1.setPrice("50");
 
-		//set up access to recipe book
+		// set up access to recipe book
 		recipeBook = cm.getRecipes();
 
-		//create hashSet for test
-		h = new HashSet<Recipe>(); 
+		// create hashSet for test
+		h = new HashSet<Recipe>();
 
-		super.setUp();		
+		super.setUp();
 	}
-	
+
 	@Test
 	public void testAddRecipe() {
 
-		//add recipe to recipe book
+		// add recipe to recipe book
 		cm.addRecipe(r1);
 
-		//check if recipe is contained within the recipe book
-		assertTrue("Recipe is in the list",Arrays.asList(recipeBook).contains(r1));
+		// check if recipe is contained within the recipe book
+		assertTrue("Recipe is in the list", Arrays.asList(recipeBook).contains(r1));
 	}
 
 	@Test
 	public void testCannotAddExistingRecipe() {
 
-		//add recipe first
+		// add recipe first
 		cm.addRecipe(r1);
 
-		//add same recipe again
+		// add same recipe again
 		cm.addRecipe(r1);
 
-		//iterate over the recipe book for duplicates
+		// iterate over the recipe book for duplicates
 		for (Recipe recipe : recipeBook) {
-			//check if there is duplicate recipes by adding each recipe into the hashset (does not allow duplicates)
-			if(h.add(recipe) == false){
+			// check if there is duplicate recipes by adding each recipe into the hashset
+			// (does not allow duplicates)
+			if (h.add(recipe) == false) {
 				fail("Duplicate recipes exist in recipebook");
 			}
 		}
 
-		//SUCCESS
+		// SUCCESS
 	}
 
 	@Test
 	public void testDeleteRecipe() {
 
-		//add recipe to recipe book
+		// add recipe to recipe book
 		cm.addRecipe(r1);
 
-		//delete recipe from recipe book from first position
+		// delete recipe from recipe book from first position
 		cm.deleteRecipe(0);
 
-		//check if recipe is removed from the list from the position specified
-		assertNull("Recipe is not in the list",recipeBook[0]);
+		// check if recipe is removed from the list from the position specified
+		assertNull("Recipe is not in the list", recipeBook[0]);
 	}
-	
+
+	@Test
+	public void testEditRecipe() {
+
+		// add recipe to recipe book
+		cm.addRecipe(r1);
+
+		// create a new recipe (which will be the content that changes the first recipe)
+		r2 = new Recipe();
+
+		// create content of recipe
+		String name = "x";
+		String amtChocolate = "1";
+		String amtCoffee = "2";
+		String amtMilk = "2";
+		String amtSugar = "2";
+		String price = "10";
+
+		// add content to recipe
+		r2.setName(name);
+		r2.setAmtChocolate(amtChocolate);
+		r2.setAmtCoffee(amtCoffee);
+		r2.setAmtMilk(amtMilk);
+		r2.setAmtSugar(amtSugar);
+		r2.setPrice(price);
+
+		// edit recipe at first position
+		cm.editRecipe(0, r2);
+
+		// Oracle: check if the first recipe contains the new field value (fails together)
+		assertAll(recipeBook[0], 
+			() -> assertEquals(name, recipeBook[0].getName()),
+			() -> assertEquals(amtChocolate, recipeBook[0].getAmtChocolate()),
+			() -> assertEquals(amtChocolate, recipeBook[0].getAmtCoffee()),
+			() -> assertEquals(amtChocolate, recipeBook[0].getAmtMilk()),
+			() -> assertEquals(amtChocolate, recipeBook[0].getAmtSugar()),
+			() -> assertEquals(amtChocolate, recipeBook[0].getPrice())
+			);
+	}
+
 	@AfterEach
-	public void tearDown() {
+	public void tearDown() throws Exception {
 		cm = null;
 		r1 = null;
+		r2 = null;
+		recipeBook = null;
+		h = null;
 	}
 
 }
